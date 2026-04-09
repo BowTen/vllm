f538359bdd7ed811b4a09a31b71805fce695fa87
bowten <bowten@qq.com>
2026-04-09 21:24:15 +0800

    Add DSSD edge draft sampler

--- vllm/dssd/edge/__init__.py
+++ vllm/dssd/edge/__init__.py
@@ -1,8 +1,10 @@
+from .sampler import DSSDEdgeDraftSampler
 from .scheduler import EdgeSchedulerAdapter
 from .state_bridge import EdgeStateBridge
 from .types import EdgeOpenSessionResult, EdgeRoundState, EdgeSession
 
 __all__ = [
+    "DSSDEdgeDraftSampler",
     "EdgeOpenSessionResult",
     "EdgeRoundState",
     "EdgeSchedulerAdapter",
